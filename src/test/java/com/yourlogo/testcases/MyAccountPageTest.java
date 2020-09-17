a399b88247165ee49deedfa1e7ce4428060cee61
Manali3004 <68749971+Manali3004@users.noreply.github.com>
2020-09-17 16:54:36 -0400

    updated new testcase

---
+++ src/test/java/com/yourlogo/testcases/MyAccountPageTest.java
@@ -0,0 +1,11 @@
+package com.yourlogo.testcases;
+
+import org.testng.annotations.Test;
+
+public class MyAccountPageTest {
+
+	@Test
+	public void test(){
+		System.out.println("i am in myaccount page");
+	}
+}
